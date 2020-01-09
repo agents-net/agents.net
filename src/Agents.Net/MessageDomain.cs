02ecd6412ff76edd4b5fa38af210a8d96d065efc
Tobias Wilker <tobias.wilker@gmail.com>
2020-01-09 09:33:50 +0100

    Add Equals implementation to MessageDomain. (#5)
    * Add Equals implementation to MessageDomain.
    
    * Add using statement.

--- src/Agents.Net/MessageDomain.cs
+++ src/Agents.Net/MessageDomain.cs
@@ -7,11 +7,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 #endregion
 
+using System;
 using System.Collections.Generic;
 
 namespace Agents.Net
 {
-    public class MessageDomain
+    public class MessageDomain : IEquatable<MessageDomain>
     {
         private readonly List<MessageDomain> children = new List<MessageDomain>();
 
@@ -41,6 +42,56 @@ namespace Agents.Net
                     return children.ToArray();
                 }
             }
+        }
+
+        public bool Equals(MessageDomain other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Equals(Root, other.Root);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((MessageDomain) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Root != null ? Root.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(MessageDomain left, MessageDomain right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(MessageDomain left, MessageDomain right)
+        {
+            return !Equals(left, right);
         }
 
         public bool IsTerminated { get; private set; }
